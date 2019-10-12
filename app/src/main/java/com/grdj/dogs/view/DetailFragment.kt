71b8dde69cfc4e5909012b3ff071eec03fc94f45
cgcman <cgcman2007@gmail.com>
2019-10-12 12:54:29 -0300

    add navigation controller

---
+++ app/src/main/java/com/grdj/dogs/view/DetailFragment.kt
@@ -0,0 +1,37 @@
+package com.grdj.dogs.view
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.NavDirections
+import androidx.navigation.Navigation
+
+import com.grdj.dogs.R
+import kotlinx.android.synthetic.main.fragment_detail.*
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class DetailFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_detail, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        buttonDetails.setOnClickListener {
+            val action : NavDirections = DetailFragmentDirections.actionListFragment()
+            Navigation.findNavController(it).navigate(action)
+        }
+    }
+
+}
